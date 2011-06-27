357d3aec0785afea9ec0be7c8c163828f730a244
Johnicholas Hines <johnicholas.hines@gmail.com>
2011-06-27 10:11:05 -0700

    Download and add lemon parser generator from Richard Hipp

---
+++ lemon.c
@@ -0,0 +1,63 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html><head>
+<meta http-equiv="content-type" content="text/html; charset=UTF-8">
+<link rel="stylesheet" type="text/css" href="/cvstrac.css" media="all">
+<title>SQLite CVSTrac</title>
+  
+</head>
+<body>
+<div><!-- container div to satisfy validator -->
+
+<div class="sqlite_header">
+<a href="/index.html">
+<img class="logo" src="/images/SQLite.gif" alt="SQLite Logo" border="0"></a>
+<div><!-- IE hack to prevent disappearing logo--></div>
+<div class="tagline">Small. Fast. Reliable.<br>Choose any three.</div>
+
+<table width=100% style="clear:both"><tr><td>
+  <div class="se"><div class="sw"><div class="ne"><div class="nw">
+  <div class="toolbar">
+    <a href="/about.html">About</a>
+    <a href="/sitemap.html">Sitemap</a>
+    <a href="/docs.html">Documentation</a>
+    <a href="/download.html">Download</a>
+    <a href="/copyright.html">License</a>
+    <a href="/news.html">News</a>
+    <a href="/cvstrac/index">Developers</a>
+    <a href="/support.html">Support</a>
+  </div></div></div></div></div>
+</td></tr></table>
+</div><div id="header">
+<h1 id="title">
+sqlite - Home Page
+</h1>
+<p id="identity">
+<a href="honeypot"><notatag arg="meaningless"></a>
+<a href="login?nxp=/cvstrac/index" title="Log in">Not logged in</a>
+</p>
+<ul id="navigation">
+<li><a href="honeypot">Honeypot</a></li>
+<li><a href="dir">Browse</a></li>
+<li id="current"><a href="index">Home</a></li>
+<li><a href="login">Login</a></li>
+<li><a href="mainmenu">Main Menu</a></li>
+<li><a href="reportlist">Reports</a></li>
+<li><a href="search">Search</a></li>
+<li><a href="timeline">Timeline</a></li>
+<li><a href="wiki">Wiki</a></li>
+</ul>
+</div>
+<div id="content">
+<strong class="two">SQLite developer links:</strong>
+
+<p><ul>
+<li><a class="external" href="http://www.sqlite.org/src/tktnew">Report A Bug</a>
+<li><a href="wiki">Wiki</a>
+<li><a class="external" href="http://www.sqlite.org/src/timeline">Timeline</a>
+<li><a class="external" href="http://www.sqlite.org/src/dir">Browse historical source files</a>
+<li><a class="external" href="http://www.sqlite.org/docsrc">Documentation Sources</a>
+
+<p><li><a href="wikiedit?p=HomePage">Edit this page</a>
+   (registered developers only)</ul>
+</div>
+</div></body></html>
