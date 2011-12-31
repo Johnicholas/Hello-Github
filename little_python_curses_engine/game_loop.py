85e00064b1a5f1de6bb223bf8cba0e25f15f5fc6
Johnicholas Hines <johnicholas.hines@gmail.com>
2011-12-31 03:38:06 -0500

    Finish the planned functionality

--- little_python_curses_engine/game_loop.py
+++ little_python_curses_engine/game_loop.py
@@ -1,6 +1,136 @@
-# todo: add a game loop here
+# based on Steven Fuerst's obfuscated, 512-byte roguelike "Monster Cave"
 
-from little_curses import addch
-addch("h")
-addch("i")
+from little_curses import *
+
+class Point:
+    def __init__(self, row, col):
+        self.row = row
+        self.col = col
+
+class MonsterCave:
+    def __init__(self):
+        self.rows = 25
+        self.cols = 80
+        self.rooms = 50
+        self.monsters = 10
+        self.map = {}
+        self.loc = {}
+        self.hp = {}
+
+    def setup(self):
+        for row in range(0, self.rows):
+            self.map[row] = {}
+            for col in range(0, self.cols):
+                self.map[row][col] = '#'
+
+        # make a messy cave by opening out a lot of rectangles
+        for i in range(0, self.rooms):
+            height = rand(3, 6)
+            width = rand(1, 25)
+            top = rand(1, self.rows - height - 1)
+            left = rand(1, self.cols - width - 1)
+            bottom = top + height
+            right = left + width
+            for row in range(top, bottom):
+                for col in range(left, right):
+                    self.map[row][col] = '.'
+
+        # place entities (player and monsters)
+        for i in range(0, self.monsters + 1):
+            while True:
+                row = rand(1, self.rows - 1)
+                col = rand(1, self.cols - 1)
+                if self.map[row][col] == '.':
+                    self.loc[i] = Point(row, col)
+                    if i == 0:
+                        # the player
+                        self.map[row][col] = '@'
+                        self.hp[i] = 5
+                    else:
+                        # a monster
+                        self.map[row][col] = 'M'
+                        self.hp[i] = 2
+                    break
+
+        # place the stairway
+        while True:
+            row = rand(1, self.rows - 1)
+            col = rand(1, self.cols - 1)
+            if self.map[row][col] == '.':
+                self.map[row][col] = '>'
+                break
+
+    def draw(self):
+        assert type(self.map) == dict
+        for row in range(0, self.rows):
+            assert type(self.map[row]) == dict
+            move(row, 0)
+            for col in range(0, self.cols):
+                assert type(self.map[row][col]) == str
+                addch(self.map[row][col])
+
+        # put the cursor on the player
+        move(self.loc[0].row, self.loc[0].col)
+
+    def update(self, key):
+        if key == err():
+            return
+        else:
+            # we've handled the special value,
+            # convert it to a character
+            key = chr(key)
+        
+        for i in range(0, self.monsters+1):
+            oldrow = self.loc[i].row
+            newrow = oldrow
+            oldcol = self.loc[i].col
+            newcol = oldcol
+            if i == 0:
+                
+                # move the player
+                if key == 'w':
+                    newrow = newrow - 1
+                elif key == 'a':
+                    newcol = newcol - 1
+                elif key == 's':
+                    newrow = newrow + 1
+                elif key == 'd':
+                    newcol = newcol + 1
+                
+                if self.map[newrow][newcol] == '>':
+                    # going down is just like starting over
+                    self.setup()
+                    return
+
+                for j in range(1, self.monsters):
+                    if self.loc[j].row == newrow and self.loc[j].col == newcol:
+                        # player bumped a monster
+                        if self.hp[j] > 0:
+                            # and it was alive at the time
+                            self.hp[j] = self.hp[j] - 1
+                            if self.hp[j] == 0:
+                                # now it is dead
+                                self.map[self.loc[j].row][self.loc[j].col] = 'D'
+            else:
+                if self.hp[i] > 0:
+                    # move a monster
+                    newrow = newrow + rand(0, 3) - 1
+                    newcol = newcol + rand(0, 3) - 1
+
+                    if newrow == self.loc[0].row and newcol == self.loc[0].col:
+                        # monster bumped the player
+                        self.hp[0] = self.hp[0] - 1
+
+                        if self.hp[0] == 0:
+                            # monster killed the player
+                            quit()
+            
+            # regardless, if an entity moved, the map needs to be updated
+            if self.map[newrow][newcol] == '.':
+                self.loc[i] = Point(newrow, newcol)
+                self.map[newrow][newcol] = self.map[oldrow][oldcol]
+                self.map[oldrow][oldcol] = '.'
+
+start(MonsterCave())
+
 
--- little_python_curses_engine/py_curses.cpp
+++ little_python_curses_engine/py_curses.cpp
@@ -1,18 +1,69 @@
 #include <Python.h>
 #include <cassert>
+#include <cmath>
 #include <cstdio>
 #include <curses.h>
 
-
 static PyObject* my_addch(PyObject* self, PyObject* args) {
   char to_add;
-  PyArg_ParseTuple(args, "c", &to_add);
+  assert(PyArg_ParseTuple(args, "c", &to_add));
   addch(to_add);
   return Py_BuildValue("");
 }
 
+static PyObject* my_rand(PyObject* self, PyObject* args) {
+  int min, max;
+  assert(PyArg_ParseTuple(args, "ii", &min, &max));
+  return Py_BuildValue("i", rand() % (max - min) + min);
+}
+
+static PyObject* my_move(PyObject* self, PyObject* args) {
+  int row, col;
+  assert(PyArg_ParseTuple(args, "ii", &row, &col));
+  move(row, col);
+  return Py_BuildValue("");
+}
+
+static PyObject* my_quit(PyObject* self, PyObject* args) {
+  endwin();
+  Py_Finalize();
+  exit(0);
+}
+
+static PyObject* my_start(PyObject* self, PyObject* args) {
+  PyObject* game;
+  assert(PyArg_ParseTuple(args, "O", &game));
+  // setup
+  PyObject* result = PyObject_CallMethod(game, "setup", "");
+  if (result == NULL) { return NULL; }
+  Py_DECREF(result);
+  while (1) {
+    // draw
+    result = PyObject_CallMethod(game, "draw", "");
+    if (result == NULL) { return NULL; }
+    Py_DECREF(result);
+    refresh();
+    // update
+    int c = getch();
+    result = PyObject_CallMethod(game, "update", "i", c);
+    if (result == NULL) { return NULL; }
+    Py_DECREF(result);
+  }
+
+  return Py_BuildValue("");
+}
+
+static PyObject* my_err(PyObject* self, PyObject* args) {
+  return Py_BuildValue("i", ERR);
+}
+
 static PyMethodDef my_curses_methods[] = {
   { "addch", my_addch, METH_VARARGS, "add a character to the screen" },
+  { "rand", my_rand, METH_VARARGS, "generate a random integer" },
+  { "move", my_move, METH_VARARGS, "move the cursor" },
+  { "quit", my_quit, METH_VARARGS, "quit the game" },
+  { "start", my_start, METH_VARARGS, "start the game" },
+  { "err", my_err, METH_VARARGS, "returns the number used by getch to indicate 'no key pressed'" },
   { 0, 0, 0, 0 }
 };
 
@@ -34,15 +85,12 @@ int main(int argc, char* argv[]) {
   // don't echo user input.
   noecho();
   // getch returns ERR if the user takes more than a tenth of a second
-  halfdelay(1);
+  // halfdelay(1);
   // don't ignore function keys F1, F2, etc.
   keypad(stdscr, TRUE);
   // TODO: seed the random number generator with the time.
-  PyRun_SimpleFile(fopen(argv[1], "r"), argv[1]);
 
-  // TODO: these two are just a hack to see something work.
-  refresh();
-  sleep(1);
+  PyRun_SimpleFile(fopen(argv[1], "r"), argv[1]);
 
   endwin();
   Py_Finalize();
