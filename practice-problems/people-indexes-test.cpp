78190d5ae247ed063935c6905ff4494bb2eaab9d
Johnicholas Hines <johnicholas.hines@gmail.com>
2020-06-02 14:50:39 -0400

    Move this version out into a golden implementation, and refactor the tests to check against the golden

--- practice-problems/people-indexes-test.cpp
+++ practice-problems/people-indexes-test.cpp
@@ -7,141 +7,27 @@
 #include <gtest/gtest.h>
 #include <gmock/gmock.h>
 
-using ::testing::ElementsAre;
+#include "golden-people-indexes.h"
 
-TEST(FooTest, CanWork1) {
-  std::vector<std::vector<std::string>> input {
+using ::testing::ElementsAre;
+using ::testing::ElementsAreArray;
+
+TEST(PeopleIndexes, GoldenImplementation) {
+  std::vector<std::vector<std::string>> big_input {
     {"leetcode","google","facebook"},
       {"google","microsoft"},
 	{"google","facebook"},
 	  {"google"},
 	    {"amazon"}
   };
-  Solution toTest;
-  EXPECT_THAT(toTest.peopleIndexes(input), ElementsAre(0, 1, 4));
+
+  std::vector<std::vector<std::string>> input;
+  for (const auto& person : big_input) {
+    input.emplace_back();
+    for (const auto& company : person) {
+      input.back().push_back(company);
+      Solution toTest;
+      EXPECT_THAT(toTest.peopleIndexes(input), ElementsAreArray(GoldenPeopleIndexes(input)));
+    }
+  }
 }
-
-
-TEST(FooTest, CanWork2) {
-  std::vector<std::vector<std::string>> input {
-    {"leetcode","google","facebook"},
-      {"google","microsoft"},
-	{"google","facebook"},
-	  {"google"},
-	    {}
-  };
-  Solution toTest;
-  EXPECT_THAT(toTest.peopleIndexes(input), ElementsAre(0, 1));
-}
-
-
-TEST(FooTest, CanWork3) {
-  std::vector<std::vector<std::string>> input {
-    {"leetcode","google","facebook"},
-      {"google","microsoft"},
-	{"google","facebook"},
-	  {"google"}
-  };
-  Solution toTest;
-  EXPECT_THAT(toTest.peopleIndexes(input), ElementsAre(0, 1));
-}
-
-TEST(FooTest, CanWork4) {
-  std::vector<std::vector<std::string>> input {
-    {"leetcode","google","facebook"},
-      {"google","microsoft"},
-	{"google","facebook"},
-	  {}
-  };
-  Solution toTest;
-  EXPECT_THAT(toTest.peopleIndexes(input), ElementsAre(0, 1));
-}
-
-
-TEST(FooTest, CanWork5) {
-  std::vector<std::vector<std::string>> input {
-    {"leetcode","google","facebook"},
-      {"google","microsoft"},
-	{"google","facebook"}
-  };
-  Solution toTest;
-  EXPECT_THAT(toTest.peopleIndexes(input), ElementsAre(0, 1));
-}
-
-TEST(FooTest, CanWork6) {
-  std::vector<std::vector<std::string>> input {
-    {"leetcode","google","facebook"},
-      {"google","microsoft"},
-	{"google"}
-  };
-  Solution toTest;
-  EXPECT_THAT(toTest.peopleIndexes(input), ElementsAre(0, 1));
-}
-
-TEST(FooTest, CanWork7) {
-  std::vector<std::vector<std::string>> input {
-    {"leetcode","google","facebook"},
-      {"google","microsoft"},
-	{}
-  };
-  Solution toTest;
-  EXPECT_THAT(toTest.peopleIndexes(input), ElementsAre(0, 1));
-}
-
-TEST(FooTest, CanWork8) {
-  std::vector<std::vector<std::string>> input {
-    {"leetcode","google","facebook"},
-      {"google","microsoft"}
-  };
-  Solution toTest;
-  EXPECT_THAT(toTest.peopleIndexes(input), ElementsAre(0, 1));
-}
-
-TEST(FooTest, CanWork9) {
-  std::vector<std::vector<std::string>> input {
-    {"leetcode","google","facebook"},
-      {}
-  };
-  Solution toTest;
-  EXPECT_THAT(toTest.peopleIndexes(input), ElementsAre(0));
-}
-
-TEST(FooTest, CanWork10) {
-  std::vector<std::vector<std::string>> input {
-    {"leetcode","google","facebook"},
-  };
-  Solution toTest;
-  EXPECT_THAT(toTest.peopleIndexes(input), ElementsAre(0));
-}
-
-TEST(FooTest, CanWork11) {
-  std::vector<std::vector<std::string>> input {
-    {"leetcode","google"},
-  };
-  Solution toTest;
-  EXPECT_THAT(toTest.peopleIndexes(input), ElementsAre(0));
-}
-
-TEST(FooTest, CanWork12) {
-  std::vector<std::vector<std::string>> input {
-    {"leetcode"},
-  };
-  Solution toTest;
-  EXPECT_THAT(toTest.peopleIndexes(input), ElementsAre(0));
-}
-
-TEST(FooTest, CanWork13) {
-  std::vector<std::vector<std::string>> input {
-    {},
-  };
-  Solution toTest;
-  EXPECT_THAT(toTest.peopleIndexes(input), ElementsAre(0));
-}
-
-TEST(FooTest, CanWork14) {
-  std::vector<std::vector<std::string>> input {};
-  Solution toTest;
-  EXPECT_THAT(toTest.peopleIndexes(input), ElementsAre());
-}
-
-
