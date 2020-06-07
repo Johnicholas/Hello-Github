c23b97b142af3e0589c65041d02654a2051a54ac
Johnicholas Hines <johnicholas.hines@gmail.com>
2020-06-07 14:09:30 -0400

    moving people-indexes into a subdirectory

---
+++ practice-problems/people-indexes/people-indexes.cpp
@@ -0,0 +1,64 @@
+#include <algorithm>
+#include <string>
+#include <vector>
+#include "people-indexes.h"
+
+using namespace std;
+
+class Problem {
+private:
+  vector<vector<string>> favorites;
+public:
+  // add a new person, with an initially empty set of favorite companies
+  // mutator
+  void addPerson() {
+    favorites.emplace_back();
+  }
+
+  // add a new favorite company, to the most recently-added person
+  // mutator
+  void addCompany(string company) {
+    favorites.back().push_back(company);
+  }
+
+  // returns true if favorites[i] is a subset of favorites[j]
+  bool isSubsetOf(int i, int j) {
+    return all_of(favorites[i].begin(), favorites[i].end(), [&](const string& companyName1) {
+	return any_of(favorites[j].begin(), favorites[j].end(), [&](const string& companyName2) {
+	    return companyName1 == companyName2;
+	  });
+      });
+  }
+
+  bool aSubsetOfAnyOther(int i) {
+    bool found = false;
+    for (int j = 0; j < favorites.size(); j++) {
+      if (i == j) {
+	continue;
+      }
+      if (isSubsetOf(i, j)) {
+	found = true;
+      }
+    }
+    return found;
+  }
+};
+
+vector<int> Solution::peopleIndexes(vector<vector<string>>& favorites) {
+  Problem p;
+  for (const auto& person : favorites) {
+    p.addPerson();
+    for (const auto& company : person) {
+      p.addCompany(company);
+    }
+  }
+      
+  vector<int> a;
+  for (int i = 0; i < favorites.size(); i++) {
+    if (!p.aSubsetOfAnyOther(i)) {
+      a.push_back(i);
+    }
+  }
+  return a;
+}
+
